3a47879a1a5191600b37113dc5c42993932674c2
cosmasobi <cosmasobi67@gmail.com>
2025-02-17 00:14:19 -0500

    made the getContactName code plus made a new class called ContactAdapter, there's nothin in it though.

--- app/src/main/java/com/example/mycontactlist/ContactDataSource.java
+++ app/src/main/java/com/example/mycontactlist/ContactDataSource.java
@@ -7,6 +7,8 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class ContactDataSource {
     private SQLiteDatabase database;
     private ContactDBHelper dbHelper;
@@ -92,6 +94,25 @@ public class ContactDataSource {
             lastId = -1;
         }
         return lastId;
+    }
+
+    public ArrayList<String> getContactName() {
+        ArrayList<String> contactNames = new ArrayList<>();
+        try {
+            String query = "Select contactname from contact";
+            Cursor cursor = database.rawQuery(query, null);
+
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                contactNames.add(cursor.getString(0));
+                cursor.moveToNext();
+            }
+            cursor.close();
+        }
+        catch (Exception e) {
+            contactNames = new ArrayList<String>();
+        }
+        return contactNames;
     }
 
 }
