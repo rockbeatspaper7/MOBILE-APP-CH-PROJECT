8653bdd455c8c15887a0fbd022d11fa2dbbc5feb
cosmasobi <cosmasobi67@gmail.com>
2025-02-26 03:24:04 -0500

    EVERYTHING WORKS DO NOT TOUCH ANYTHING FOR THE LOVE OF GOD PLEEEEAAASSSEEEE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.play.services.maps)
-    implementation(libs.play.services.maps.license)
-    implementation(libs.play.services)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
